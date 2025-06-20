661a620674c1348e6312541a87760ee4624eeb4f
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 20:41:21 +0000

    [automatic commit] 2025-06-20 20:41:21 UTC 122.150.235.99

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser'); // Added this line
+const session = require('express-session');    // Added this line
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser()); // Added this middleware
+
+// Session configuration added here
+app.use(session({
+    secret: 'a_secret_key_for_the_dog_walking_app', // This is just a random secret string
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
